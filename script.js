136bd631de78dc31a0d6c872432ab0a27ea8d0dd
Sally077 <sallyhedges77@gmail.com>
2023-01-29 07:47:46 +0000

    added local storage to textarea test

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 var today = moment();
 $("#currentDay").text(today.format("D MMM YYYY"));
 
+var form = document.querySelector("form")
+var getText = document.querySelector(".enterText");
+var saveBtn = document.querySelector("#savedata");
+
+var h1 = document.querySelector("h1");
+var note = document.querySelector(".dataStored");
+var p = document.querySelector(".Saved");
+
+
+
+
+// Event Listener
+
+form.addEventListener("save", function(event) {
+    event.preventDefault()
+
+});
+
+saveBtn.addEventListener('click', function(){
+
+    localStorage.setItem('text', enterText.value);
+    nameDisplayCheck();
+})
+
+function nameDisplayCheck() {
+    if (localStorage.getItem('text')) {
+        var text = localStorage.getItem(text);
+        h1.textContent = `Event Saved`
+        saved.textContent = `Now saved to local storage`;
+
+    }
+}console.log(localStorage.getItem('txt'))
+
+
+
+
+
+
